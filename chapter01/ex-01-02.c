019650314a293394ac3a391742da56fa15bc6a8b
Martin Homuth <martin.homuth@emlix.com>
2017-12-03 17:49:55 +0100

    add exercise 01-02
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

---
+++ chapter01/ex-01-02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main()
+{
+	printf("\\a: \a\n");
+	printf("\\b: \b\n");
+	printf("\\c: \c\n");
+	printf("\\d: \d\n");
+	printf("\\e: \e\n");
+	printf("\\f: \f\n");
+	printf("\\g: \g\n");
+	printf("\\h: \h\n");
+	printf("\\i: \i\n");
+	printf("\\j: \j\n");
+	printf("\\k: \k\n");
+	printf("\\l: \l\n");
+}
